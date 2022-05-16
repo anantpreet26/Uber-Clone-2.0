fd8fc6ae2a4e88e7befa3f99c294bdcc5341a811
Anant Preet Singh Reen <reachout2anant@gmail.com>
2022-05-17 03:09:38 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",],
+  "./components/**/*.{js,ts,jsx,tsx}",
+  "./context/**/*.{js,ts,jsx,tsx}",
+  "./api/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
